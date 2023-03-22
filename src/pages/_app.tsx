24dc9ce9d380fabcf3f16a9b4d535be30ec5aac4
Dusty McCord <dustymccord@gmail.com>
2023-03-21 20:38:08 -0700

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Box } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Box>Test</Box>
+    </div>
+  );
+};
+
+export default Home;
